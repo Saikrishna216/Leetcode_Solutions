2b3bd9dc029819905d1977faced5aefdf43045e5
Saikrishna216 <krishnaksai216@gmail.com>
2024-12-24 19:21:46 +0530

    Problem Difficulty Level : Medium

---
+++ Tree/513. Find Bottom Left Tree Value.py
@@ -0,0 +1,37 @@
+
+    """
+    Time complexity : O(logn)
+    Space complexity : O(n)
+    """
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        queue = deque([root])
+        leftmost_value = None
+
+        while queue:
+            node = queue.popleft()
+
+            leftmost_value = node.val
+
+            if node.right:
+                queue.append(node.right)
+            if node.left:
+                queue.append(node.left)
+
+        return leftmost_value
+
+
+    """
+    Author : Saikrishna216
+    Question Link : https://leetcode.com/problems/find-bottom-left-tree-value/
+    """
+
+    
