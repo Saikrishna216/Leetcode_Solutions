886ed1ca8fbd71eed81b424b3563e7ad6e805358
Saikrishna216 <krishnaksai216@gmail.com>
2025-02-22 23:45:16 +0530

    Problem Difficulty Level : medium

---
+++ Stack/1249. Minimum Remove to Make Valid Parentheses.py
@@ -0,0 +1,54 @@
+
+    """
+    Time complexity : O(n)
+    Space complexity : O(1)
+    """
+    
+    
+    """
+    Additional Notes : nil
+    """
+
+    
+    class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        open_count = 0
+        close_count = 0
+        flag = 0
+        ans = ""
+        
+        for char in s:
+            if char == '(':
+                open_count += 1
+                flag += 1
+            elif char == ')' and flag > 0:
+                close_count += 1
+                flag -= 1
+        
+        k = min(open_count, close_count)
+        open_count = k
+        close_count = k
+        
+        for char in s:
+            if char == '(':
+                if open_count > 0:
+                    ans += '('
+                    open_count -= 1
+                continue
+            if char == ')':
+                if close_count > 0 and close_count > open_count:
+                    ans += ')'
+                    close_count -= 1
+                continue
+            else:
+                ans += char
+        
+        return ans
+    
+    
+    """
+    Author : Saikrishna216
+    Question Link : https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/submissions/1225015697/
+    """
+    
+    
