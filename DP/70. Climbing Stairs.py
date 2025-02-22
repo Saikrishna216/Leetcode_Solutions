3529301299f567e899dcf6e4891880bac8307dc0
Saikrishna216 <krishnaksai216@gmail.com>
2025-02-23 00:13:27 +0530

    Problem Difficulty Level : Easy

---
+++ DP/70. Climbing Stairs.py
@@ -0,0 +1,31 @@
+
+    """
+    Time complexity : O(n)
+    Space complexity : O(1)
+    """
+    
+    
+    """
+    Additional Notes : nil
+    """
+
+    
+    class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 1:
+            return 1
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
+
+    
+    
+    """
+    Author : Saikrishna216
+    Question Link : https://leetcode.com/problems/climbing-stairs/
+    """
+    
+    
