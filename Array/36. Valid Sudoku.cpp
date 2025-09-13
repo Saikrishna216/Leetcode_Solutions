b40a187a5943a8c404c51a03ad89fc814f7fa6b0
Sai Krishna <115548367+Saikrishna216@users.noreply.github.com>
2025-09-13 14:36:31 +0530

    Problem Difficultly Level: Medium

---
+++ Array/36. Valid Sudoku.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<set<char>> row(9),col(9),boxes(9);
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.') continue;
+                if(row[i].count(board[i][j])) return false;
+                row[i].insert(board[i][j]);
+
+                if(col[j].count(board[i][j]))return false;
+                col[j].insert(board[i][j]);
+
+                int idx=(i/3)*3+(j/3);
+                if(boxes[idx].count(board[i][j]))return false;
+                boxes[idx].insert(board[i][j]);
+            }
+        }
+        return true;
+    }
+};
