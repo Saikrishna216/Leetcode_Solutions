b9ec4da20b27d036a7a975224e425a2feff5c9b4
Sai Krishna <115548367+Saikrishna216@users.noreply.github.com>
2026-04-30 21:40:18 +0530

    Problem Difficultly Level: Medium

---
+++ Array/1877. Minimize Maximum Pair Sum in Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int l=0;
+        int r=nums.size()-1;
+        int ans=0;
+        for(int i=0;i<=nums.size()/2;i++){
+            ans=max(ans,nums[l]+nums[r]);
+            l+=1;
+            r-=1;
+        }
+        return ans;
+    }
+};
