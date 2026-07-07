3353e91facab56c2c5fcfa7021bf75fa6119700e
Sai Krishna <115548367+Saikrishna216@users.noreply.github.com>
2026-07-07 12:22:50 +0530

    Problem Difficultly Level: Medium

---
+++ BFS/994. Rotting Oranges.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m= grid.size();
+        int n = grid[0].size();
+        queue<pair<int,int>> q;
+        int fresh=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2) q.push({i,j});
+                if(grid[i][j]==1) fresh++;
+            }
+        }
+        if(fresh==0) return 0;
+        if(q.empty()) return -1;
+        vector<pair<int,int>> dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+        int ans=0;
+        while(!q.empty()){
+            int sz=q.size();
+            while(sz--){
+                auto [x,y]=q.front();
+                q.pop();
+                for (auto [dx, dy] : dirs) {
+                    int i = x + dx;
+                    int j = y + dy;
+                    if (i >= 0 && i < m && j >= 0 && j < n && grid[i][j] == 1) {
+                        grid[i][j] = 2;
+                        fresh--;
+                        q.push({i, j});
+                    }
+                }
+            }
+            ans++;
+        }
+        return fresh==0?ans-1:-1;
+    }
+};
